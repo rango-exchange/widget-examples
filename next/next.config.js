239c30a230173f1f63762e839dc21cc11942eb59
samobasquiat <samo.basquiat@rango.exchange>
2023-05-28 14:58:04 +0000

    fix: add widget to next.js

--- next/next.config.js
+++ next/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const withTM = require('next-transpile-modules')(['rango-types' , '@rango-dev/widget-embedded']); // pass the modules you would like to see transpiled
-
 const nextConfig = {
-  // reactStrictMode: true,
-  transpilePackages: ['rango-types' , '@rango-dev/widget-embedded']
-
+  experimental: {
+    appDir: true,
+  },
 }
 
-module.exports = withTM(nextConfig)
+module.exports = nextConfig
---
+++ next/src/app/page.tsx
@@ -0,0 +1,13 @@
+import RangoWidget from '../rango-widget'
+import styles from './page.module.css'
+
+function Home() {
+  return (
+    <main className={styles.main}>
+      <RangoWidget />
+    </main>
+  )
+}
+
+
+export default Home;
