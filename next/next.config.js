6390fbbfe0cb2f5d32c79388bc4378c6c543c230
RanGojo <rangojo@rango.exchange>
2023-10-25 23:39:30 +0300

    fix: fix nextjs example with latest version

--- next/next.config.js
+++ next/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
