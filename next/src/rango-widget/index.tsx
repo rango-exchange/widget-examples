a799a82c50cc6fbec4ac0f404c5f88a0d3957575
RanGojo <rangojo@rango.exchange>
2023-10-26 14:51:45 +0300

    fix: update next example

--- next/src/rango-widget/index.tsx
+++ next/src/rango-widget/index.tsx
@@ -5,7 +5,10 @@ import { Widget } from "@rango-dev/widget-embedded";
 
 function RangoWidget() {
   return <NoSSR>
-    <Widget/> 
+    <Widget config={{
+      apiKey: "c6381a79-2817-4602-83bf-6a641a409e32", 
+      walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b"
+    }}/> 
   </NoSSR>
 }
 
