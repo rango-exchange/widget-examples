08ecf9444696a8b89774f39dfb06d3f9a542f638
Shinji <shinji@rango.exchange>
2024-02-06 08:53:14 +0000

    chore: update widget-embedded version

--- next/src/rango-widget/index.tsx
+++ next/src/rango-widget/index.tsx
@@ -4,12 +4,17 @@ import NoSSR from "react-no-ssr";
 import { Widget } from "@rango-dev/widget-embedded";
 
 function RangoWidget() {
-  return <NoSSR>
-    <Widget config={{
-      apiKey: "c6381a79-2817-4602-83bf-6a641a409e32", 
-      walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b"
-    }}/> 
-  </NoSSR>
+  return (
+    //@ts-ignore
+    <NoSSR>
+      <Widget
+        config={{
+          apiKey: "c6381a79-2817-4602-83bf-6a641a409e32",
+          walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b",
+        }}
+      />
+    </NoSSR>
+  );
 }
 
 export default RangoWidget;
