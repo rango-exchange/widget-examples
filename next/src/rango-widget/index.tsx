7bd80125ea7e5699700c7afad537d7e9c17d040a
RyukTheCoder <154742563+RyukTheCoder@users.noreply.github.com>
2024-02-14 11:26:50 +0330

    chore: update Next example to use next-dynamic (#7)

--- next/src/rango-widget/index.tsx
+++ next/src/rango-widget/index.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import NoSSR from "react-no-ssr";
-import { Widget } from "@rango-dev/widget-embedded";
+import dynamic from "next/dynamic";
+
+const Widget = dynamic(
+  () => import("@rango-dev/widget-embedded").then((module) => module.Widget),
+  {
+    ssr: false,
+  }
+);
 
 function RangoWidget() {
   return (
-    //@ts-ignore
-    <NoSSR>
-      <Widget
-        config={{
-          apiKey: "c6381a79-2817-4602-83bf-6a641a409e32",
-          walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b",
-        }}
-      />
-    </NoSSR>
+    <Widget
+      config={{
+        apiKey: "c6381a79-2817-4602-83bf-6a641a409e32",
+        walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b",
+      }}
+    />
   );
 }
 
