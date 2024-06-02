42ad03f4b589b59bdcd12243e4d17659d04584d7
RyukTheCoder <ryuk@rango.exchange>
2024-06-02 11:43:53 +0330

    chore: update examples to latest version

--- parcel/src/index.tsx
+++ parcel/src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Widget } from '@rango-dev/widget-embedded';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Widget } from "@rango-dev/widget-embedded";
 
 const App = () => {
   return (
-      <Widget />
+    <Widget
+      config={{
+        apiKey: "c6381a79-2817-4602-83bf-6a641a409e32",
+        walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b",
+      }}
+    />
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
