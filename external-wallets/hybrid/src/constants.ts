0ac85a83ef5c13f79a45e633193e2a8a4f193c30
RyukTheCoder <ryuk@rango.exchange>
2024-06-02 10:13:53 +0200

    chore: update examples to latest version

--- external-wallets/hybrid/src/constants.ts
+++ external-wallets/hybrid/src/constants.ts
@@ -7,4 +7,6 @@ export const WIDGET_CONFIG: WidgetConfig = {
   wallets: providers,
   // This API key is only for test purpose. Don't use it in production.
   apiKey: "c6381a79-2817-4602-83bf-6a641a409e32",
+
+  walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b",
 };
--- external-wallets/rango-wallets/src/constants.js
+++ external-wallets/rango-wallets/src/constants.js
@@ -6,4 +6,6 @@ export const WIDGET_CONFIG = {
   wallets: WALLETS,
   // This API key is only for test purpose. Don't use it in production.
   apiKey: "c6381a79-2817-4602-83bf-6a641a409e32",
+
+  walletConnectProjectId: "e24844c5deb5193c1c14840a7af6a40b",
 };
